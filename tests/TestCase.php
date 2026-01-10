20703f81d9902daad0b27af885545fe0126910d4
ChristianDelvianto <github.placeholder.delvin@gmail.com>
2026-01-10 11:52:53 +0700

    test: add withoutVite() and undo fake vite globally

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,15 +2,9 @@
 
 namespace Tests;
 
-use Illuminate\Support\Facades\Vite;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        Vite::fake();
-    }
+    //
 }
